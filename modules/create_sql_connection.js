8fb459ede547160b9321da78c0d11f08f0840e27
thevasanthram <vasanthram227@gmail.com>
2023-09-21 15:34:29 +0530

    included pinnacle config

--- modules/create_sql_connection.js
+++ modules/create_sql_connection.js
@@ -2,11 +2,22 @@ const sql = require("mssql");
 
 async function create_sql_connection() {
   // database configuration
+  // const config = {
+  //   user: "deevia",
+  //   password: "kiran@123",
+  //   server: "deevia-trial.database.windows.net",
+  //   database: "hvac_db",
+  //   options: {
+  //     encrypt: true, // Use this option for SSL encryption
+  //     requestTimeout: 48 * 60 * 60 * 1000, // 60 seconds (adjust as needed)
+  //   },
+  // };
+
   const config = {
-    user: "deevia",
-    password: "kiran@123",
-    server: "deevia-trial.database.windows.net",
-    database: "hvac_db",
+    user: "pinnacleadmin",
+    password: "PiTestBi01",
+    server: "pinnaclemep.database.windows.net",
+    database: "bi_play_ground",
     options: {
       encrypt: true, // Use this option for SSL encryption
       requestTimeout: 48 * 60 * 60 * 1000, // 60 seconds (adjust as needed)
--- modules/create_sql_pool.js
+++ modules/create_sql_pool.js
@@ -2,11 +2,19 @@ const mssql = require("mssql");
 
 async function create_sql_connection_pool() {
   // Create a connection pool
+  // const pool = await new mssql.ConnectionPool({
+  //   user: "deevia",
+  //   password: "kiran@123",
+  //   server: "deevia-trial.database.windows.net",
+  //   database: "hvac_db",
+  //   requestTimeout: 60000 * 60 * 24, // Set the request timeout to 60 seconds (adjust as needed)
+  // }).connect();
+
   const pool = await new mssql.ConnectionPool({
-    user: "deevia",
-    password: "kiran@123",
-    server: "deevia-trial.database.windows.net",
-    database: "hvac_db",
+    user: "pinnacleadmin",
+    password: "PiTestBi01",
+    server: "pinnaclemep.database.windows.net",
+    database: "bi_play_ground",
     requestTimeout: 60000 * 60 * 24, // Set the request timeout to 60 seconds (adjust as needed)
   }).connect();
 
