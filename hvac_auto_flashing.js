48260501e90e04687b992554e7148e64bbb23700
thevasanthram <vasanthram227@gmail.com>
2023-10-18 13:08:22 +0530

    new

--- hvac_auto_flashing.js
+++ hvac_auto_flashing.js
@@ -3832,12 +3832,17 @@ async function auto_update() {
   const now = new Date();
   const currentHour = now.getHours();
 
+  console.log("currentHour: ", currentHour);
+  console.log("previous_batch_next_hour: ", previous_batch_next_hour);
+
+  console.log("condition: ", currentHour != previous_batch_next_hour);
+
   // Check if it's the next hour
-  if (currentHour !== previous_batch_next_hour) {
+  if (currentHour != previous_batch_next_hour) {
     // Schedule the next call after an hour
     const timeUntilNextHour = (60 - now.getMinutes()) * 60 * 1000; // Calculate milliseconds until the next hour
     console.log("timer funtion entering", timeUntilNextHour);
-    setTimeout(auto_update, timeUntilNextHour);
+    setTimeout(auto_update, timeUntilNextHour + 60000);
   } else {
     console.log("next batch initiated");
     await flush_data_pool();
--- test4.js
+++ test4.js
@@ -1,3 +1,25 @@
-setTimeout(() => {
-  console.log("--------");
-}, 3000);
+function auto_update() {
+  console.log("enteringg");
+  // Get the current date and time
+  const previous_batch_time = new Date("2023-10-18T13:00:00.000Z");
+  const previous_batch_hour = previous_batch_time.getHours();
+
+  // Calculate the next hour
+  const previous_batch_next_hour = (previous_batch_hour + 1) % 24;
+
+  const now = new Date();
+  const currentHour = now.getHours();
+
+  // Check if it's the next hour
+  if (currentHour != previous_batch_next_hour) {
+    // Schedule the next call after an hour
+    const timeUntilNextHour = (60 - now.getMinutes()) * 60 * 1000; // Calculate milliseconds until the next hour
+    console.log("timer funtion entering", timeUntilNextHour);
+    setTimeout(auto_update, timeUntilNextHour);
+  } else {
+    console.log("next batch initiated");
+  }
+}
+
+// auto_update();
+setTimeout(auto_update, 30000);
