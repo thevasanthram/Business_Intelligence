2aca60f2239a1e3dbcdc911c6554c04994b86897
thevasanthram <vasanthram227@gmail.com>
2023-10-31 03:22:57 +0530

    flat_tables

--- main.js
+++ main.js
@@ -154,202 +154,202 @@ const api_collection = [
   //   api_group: "jbce",
   //   api_name: "call-reasons",
   // },
-  {
-    api_group: "jpm",
-    api_name: "appointments", // end keyword is used in this api response
-  },
-  {
-    api_group: "jpm",
-    api_name: "job-types",
-  },
-  {
-    api_group: "jpm",
-    api_name: "jobs",
-  },
-  {
-    api_group: "jpm",
-    api_name: "projects",
-  },
-  {
-    api_group: "jpm",
-    api_name: "job-cancel-reasons",
-  },
-  {
-    api_group: "jpm",
-    api_name: "job-hold-reasons",
-  },
-  {
-    api_group: "jpm",
-    api_name: "project-statuses",
-  },
-  {
-    api_group: "jpm",
-    api_name: "project-substatuses",
-  },
-  {
-    api_group: "marketing",
-    api_name: "campaigns",
-  },
-  {
-    api_group: "marketing",
-    api_name: "categories",
-  },
-  {
-    api_group: "marketing",
-    api_name: "suppressions",
-  },
-  {
-    api_group: "marketing",
-    api_name: "costs",
-  },
-  {
-    api_group: "marketingreputation",
-    api_name: "reviews",
-  },
-  {
-    api_group: "memberships",
-    api_name: "memberships",
-  },
-  {
-    api_group: "memberships",
-    api_name: "recurring-services",
-  },
-  {
-    api_group: "memberships",
-    api_name: "recurring-service-events",
-  },
-  {
-    api_group: "memberships",
-    api_name: "recurring-service-types",
-  },
-  {
-    api_group: "memberships",
-    api_name: "membership-types",
-  },
-  {
-    api_group: "payroll",
-    api_name: "payrolls",
-  },
-  {
-    api_group: "payroll",
-    api_name: "payroll-adjustments",
-  },
-  {
-    api_group: "payroll",
-    api_name: "gross-pay-items",
-  },
-  {
-    api_group: "payroll",
-    api_name: "jobs/splits",
-  },
-  {
-    api_group: "payroll",
-    api_name: "jobs/timesheets",
-  },
-  {
-    api_group: "payroll",
-    api_name: "timesheet-codes",
-  },
-  {
-    api_group: "payroll",
-    api_name: "activity-codes",
-  },
-  {
-    api_group: "payroll",
-    api_name: "locations/rates",
-  },
-  {
-    api_group: "payroll",
-    api_name: "non-job-timesheets",
-  },
-  {
-    api_group: "pricebook",
-    api_name: "categories",
-  },
-  {
-    api_group: "pricebook",
-    api_name: "equipment",
-  },
-  {
-    api_group: "pricebook",
-    api_name: "materials",
-  },
-  {
-    api_group: "pricebook",
-    api_name: "discounts-and-fees",
-  },
-  {
-    api_group: "pricebook",
-    api_name: "images",
-  },
-  {
-    api_group: "pricebook",
-    api_name: "services",
-  },
-  {
-    api_group: "pricebook",
-    api_name: "materialsmarkup",
-  },
   // {
-  //   api_group: "reporting",
-  //   api_name: "report-categories",
+  //   api_group: "jpm",
+  //   api_name: "appointments", // end keyword is used in this api response
   // },
   // {
-  //   api_group: "sales",
-  //   api_name: "estimates",
+  //   api_group: "jpm",
+  //   api_name: "job-types",
   // },
+  // {
+  //   api_group: "jpm",
+  //   api_name: "jobs",
+  // },
+  // {
+  //   api_group: "jpm",
+  //   api_name: "projects",
+  // },
+  // {
+  //   api_group: "jpm",
+  //   api_name: "job-cancel-reasons",
+  // },
+  // {
+  //   api_group: "jpm",
+  //   api_name: "job-hold-reasons",
+  // },
+  // {
+  //   api_group: "jpm",
+  //   api_name: "project-statuses",
+  // },
+  // {
+  //   api_group: "jpm",
+  //   api_name: "project-substatuses",
+  // },
+  // {
+  //   api_group: "marketing",
+  //   api_name: "campaigns",
+  // },
+  // {
+  //   api_group: "marketing",
+  //   api_name: "categories",
+  // },
+  // {
+  //   api_group: "marketing",
+  //   api_name: "suppressions",
+  // },
+  // {
+  //   api_group: "marketing",
+  //   api_name: "costs",
+  // },
+  // {
+  //   api_group: "marketingreputation",
+  //   api_name: "reviews",
+  // },
+  // {
+  //   api_group: "memberships",
+  //   api_name: "memberships",
+  // },
+  // {
+  //   api_group: "memberships",
+  //   api_name: "recurring-services",
+  // },
+  // {
+  //   api_group: "memberships",
+  //   api_name: "recurring-service-events",
+  // },
+  // {
+  //   api_group: "memberships",
+  //   api_name: "recurring-service-types",
+  // },
+  // {
+  //   api_group: "memberships",
+  //   api_name: "membership-types",
+  // },
+  // {
+  //   api_group: "payroll",
+  //   api_name: "payrolls",
+  // },
+  // {
+  //   api_group: "payroll",
+  //   api_name: "payroll-adjustments",
+  // },
+  // {
+  //   api_group: "payroll",
+  //   api_name: "gross-pay-items",
+  // },
+  // {
+  //   api_group: "payroll",
+  //   api_name: "jobs/splits",
+  // },
+  // {
+  //   api_group: "payroll",
+  //   api_name: "jobs/timesheets",
+  // },
+  // {
+  //   api_group: "payroll",
+  //   api_name: "timesheet-codes",
+  // },
+  // {
+  //   api_group: "payroll",
+  //   api_name: "activity-codes",
+  // },
+  // {
+  //   api_group: "payroll",
+  //   api_name: "locations/rates",
+  // },
+  // {
+  //   api_group: "payroll",
+  //   api_name: "non-job-timesheets",
+  // },
+  // {
+  //   api_group: "pricebook",
+  //   api_name: "categories",
+  // },
+  // {
+  //   api_group: "pricebook",
+  //   api_name: "equipment",
+  // },
+  // {
+  //   api_group: "pricebook",
+  //   api_name: "materials",
+  // },
+  // {
+  //   api_group: "pricebook",
+  //   api_name: "discounts-and-fees",
+  // },
+  // {
+  //   api_group: "pricebook",
+  //   api_name: "images",
+  // },
+  // {
+  //   api_group: "pricebook",
+  //   api_name: "services",
+  // },
+  // {
+  //   api_group: "pricebook",
+  //   api_name: "materialsmarkup",
+  // },
+  {
+    api_group: "reporting",
+    api_name: "report-categories",
+  },
+  {
+    api_group: "sales",
+    api_name: "estimates",
+  },
   // {
   //   api_group: "sales",
   //   api_name: "estimates/export",
   // },
-  // {
-  //   api_group: "sales",
-  //   api_name: "estimates/items",
-  // },
-  // {
-  //   api_group: "service-agreements",
-  //   api_name: "service-agreements",
-  // },
-  // {
-  //   api_group: "settings",
-  //   api_name: "business-units",
-  // },
-  // {
-  //   api_group: "settings",
-  //   api_name: "employees",
-  // },
-  // {
-  //   api_group: "settings",
-  //   api_name: "technicians",
-  // },
-  // {
-  //   api_group: "settings",
-  //   api_name: "tag-types",
-  // },
-  // {
-  //   api_group: "settings",
-  //   api_name: "user-roles",
-  // },
-  // {
-  //   api_group: "taskmanagement",
-  //   api_name: "data",
-  // },
+  {
+    api_group: "sales",
+    api_name: "estimates/items",
+  },
+  {
+    api_group: "service-agreements",
+    api_name: "service-agreements",
+  },
+  {
+    api_group: "settings",
+    api_name: "business-units",
+  },
+  {
+    api_group: "settings",
+    api_name: "employees",
+  },
+  {
+    api_group: "settings",
+    api_name: "technicians",
+  },
+  {
+    api_group: "settings",
+    api_name: "tag-types",
+  },
+  {
+    api_group: "settings",
+    api_name: "user-roles",
+  },
+  {
+    api_group: "taskmanagement",
+    api_name: "data",
+  },
   // {
   //   api_group: "telecom",
   //   api_name: "export/calls",
   // },
-  // {
-  //   api_group: "telecom",
-  //   api_name: "calls",
-  // },
-  // {
-  //   api_group: "forms",
-  //   api_name: "forms",
-  // },
-  // {
-  //   api_group: "forms",
-  //   api_name: "submissions",
-  // },
+  {
+    api_group: "telecom",
+    api_name: "calls",
+  },
+  {
+    api_group: "forms",
+    api_name: "forms",
+  },
+  {
+    api_group: "forms",
+    api_name: "submissions",
+  },
 ];
 
 const instance_details = [
