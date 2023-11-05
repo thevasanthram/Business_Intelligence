39a16ccf8b3cfa0d1d6af7ff7872199eed578b96
thevasanthram <vasanthram227@gmail.com>
2023-11-05 19:18:03 +0530

    call details_ bug resolved

--- hvac_auto_flashing.js
+++ hvac_auto_flashing.js
@@ -901,10 +901,10 @@ const hvac_tables = {
         data_type: "DATETIME2",
         constraint: { nullable: true },
       },
-      duration: {
-        data_type: "TIME",
-        constraint: { nullable: true },
-      },
+      // duration: {
+      //   data_type: "TIME",
+      //   constraint: { nullable: true },
+      // },
       from: {
         data_type: "NVARCHAR",
         constraint: { nullable: true },
@@ -4204,9 +4204,9 @@ async function data_processor(data_lake, sql_request, table_list) {
           project_id: record["projectId"] ? record["projectId"] : 0,
           createdOn: createdOn,
           modifiedOn: modifiedOn,
-          duration: record["leadCall"]["duration"]
-            ? record["leadCall"]["duration"]
-            : "00:00:00",
+          // duration: record["leadCall"]["duration"]
+          //   ? record["leadCall"]["duration"]
+          //   : "00:00:00",
           from: record["leadCall"]["from"]
             ? record["leadCall"]["from"]
             : "default",
