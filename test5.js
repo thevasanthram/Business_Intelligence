dbd4cf7a7492e05acef35c6f1499ed6f717992d6
thevasanthram <vasanthram227@gmail.com>
2023-11-28 15:35:52 +0530

    test5

--- test5.js
+++ test5.js
@@ -238,7 +238,7 @@ async function starter() {
     hvac_tables
   );
 
-  const first_table = data_lake["bookings"]["crm__bookings"]["data_pool"];
+  const first_table = data_lake["bookings"]["telecom__calls"]["data_pool"];
   const comparing_table1 = data_lake["bookings"]["jpm__jobs"]["data_pool"];
   const comparing_table2 =
     data_lake["bookings"]["sales__estimates"]["data_pool"];
@@ -258,8 +258,8 @@ async function starter() {
       Object.keys(first_table).map((record_id) => {
         const record = first_table[record_id];
 
-        if (record["jobId"]) {
-          return record["jobId"];
+        if (record["jobNumber"]) {
+          return record["jobNumber"];
         }
       })
     ),
