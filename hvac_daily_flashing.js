39c1746ceb0358af77bbbcc7ee25402e8147a7fa
thevasanthram <vasanthram227@gmail.com>
2024-01-04 19:23:43 +0530

    deleted some data pool after processing it

--- hvac_daily_flashing.js
+++ hvac_daily_flashing.js
@@ -62,7 +62,7 @@ createdBeforeTime.setUTCHours(7, 0, 0, 0);
 
 const params_header = {
   createdOnOrAfter: "", // 2023-12-25T00:00:00.00Z
-  createdBefore: createdBeforeTime.toISOString(), //createdBeforeTime.toISOString()
+  createdBefore: "2024-01-01T00:00:00.00Z", //createdBeforeTime.toISOString()
   includeTotal: true,
   pageSize: 2000,
   active: "any",
