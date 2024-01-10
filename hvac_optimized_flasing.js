22038d37c77ce17fd90b44bec4af4e8a36174a02
thevasanthram <vasanthram227@gmail.com>
2024-01-10 15:37:57 +0530

    optimization

--- hvac_optimized_flasing.js
+++ hvac_optimized_flasing.js
@@ -77,34 +77,6 @@ let data_lake = {};
 let should_auto_update = false;
 
 const hvac_tables = {
-  gross_pay_items: {
-    columns: {
-      id: {
-        data_type: "INT",
-        constraint: { nullable: true },
-      },
-      payrollId: {
-        data_type: "INT",
-        constraint: { nullable: true },
-      },
-      amount: {
-        data_type: "DECIMAL",
-        constraint: { nullable: true },
-      },
-      paidDurationHours: {
-        data_type: "DECIMAL",
-        constraint: { nullable: true },
-      },
-      projectId: {
-        data_type: "INT",
-        constraint: { nullable: true },
-      },
-      invoiceId: {
-        data_type: "INT",
-        constraint: { nullable: true },
-      },
-    },
-  },
   legal_entity: {
     // manual entry
     columns: {
@@ -420,7 +392,34 @@ const hvac_tables = {
       },
     },
   },
-
+  gross_pay_items: {
+    columns: {
+      id: {
+        data_type: "INT",
+        constraint: { nullable: true },
+      },
+      payrollId: {
+        data_type: "INT",
+        constraint: { nullable: true },
+      },
+      amount: {
+        data_type: "DECIMAL",
+        constraint: { nullable: true },
+      },
+      paidDurationHours: {
+        data_type: "DECIMAL",
+        constraint: { nullable: true },
+      },
+      projectId: {
+        data_type: "INT",
+        constraint: { nullable: true },
+      },
+      invoiceId: {
+        data_type: "INT",
+        constraint: { nullable: true },
+      },
+    },
+  },
   payrolls: {
     columns: {
       id: {
@@ -8224,51 +8223,54 @@ async function auto_update() {
 async function orchestrate() {
   await flush_data_pool(!should_auto_update);
 
-  do {
-    const now = new Date();
-    now.setUTCHours(7, 0, 0, 0);
+  // Step 1: Call start_pipeline
+  await start_pipeline();
 
-    // Step 1: Call start_pipeline
-    await start_pipeline();
+  // do {
+  //   const now = new Date();
+  //   now.setUTCHours(7, 0, 0, 0);
 
-    // break;
+  //   // Step 1: Call start_pipeline
+  //   await start_pipeline();
 
-    // Step 2: Check year difference
-    params_header["modifiedOnOrAfter"] = params_header["modifiedBefore"];
-    const next_initial_batch_time = new Date(params_header["modifiedBefore"]);
+  //   // break;
 
-    const yearDifference =
-      next_initial_batch_time.getFullYear() - now.getFullYear();
+  //   // Step 2: Check year difference
+  //   params_header["modifiedOnOrAfter"] = params_header["modifiedBefore"];
+  //   const next_initial_batch_time = new Date(params_header["modifiedBefore"]);
 
-    if (yearDifference >= 24) {
-      // If the difference is 24 years or more, add 24 years to modifiedOnOrAfter
-      next_initial_batch_time.setFullYear(
-        next_initial_batch_time.getFullYear() + 24
-      );
-      params_header["modifiedBefore"] = next_initial_batch_time.toISOString();
-      should_auto_update = true;
-    } else {
-      // If the difference is less than 24 years, set modifiedOnOrAfter to the current date and time
-      params_header["modifiedBefore"] = now.toISOString();
+  //   const yearDifference =
+  //     next_initial_batch_time.getFullYear() - now.getFullYear();
 
-      // Check if next_initial_batch_time is greater than the next day at 7:00 AM UTC
-      const nextDay = new Date(now);
-      nextDay.setDate(nextDay.getDate() + 1);
-      nextDay.setUTCHours(7, 0, 0, 0);
+  //   if (yearDifference >= 24) {
+  //     // If the difference is 24 years or more, add 24 years to modifiedOnOrAfter
+  //     next_initial_batch_time.setFullYear(
+  //       next_initial_batch_time.getFullYear() + 24
+  //     );
+  //     params_header["modifiedBefore"] = next_initial_batch_time.toISOString();
+  //     should_auto_update = true;
+  //   } else {
+  //     // If the difference is less than 24 years, set modifiedOnOrAfter to the current date and time
+  //     params_header["modifiedBefore"] = now.toISOString();
 
-      if (next_initial_batch_time > nextDay) {
-        // Wait until the next day at 7:00 AM UTC
-        const timeUntilNextDay = nextDay - now;
-        console.log("Waiting until the next day at 7:00 AM UTC...");
-        await new Promise((resolve) => setTimeout(resolve, timeUntilNextDay));
-      }
+  //     // Check if next_initial_batch_time is greater than the next day at 7:00 AM UTC
+  //     const nextDay = new Date(now);
+  //     nextDay.setDate(nextDay.getDate() + 1);
+  //     nextDay.setUTCHours(7, 0, 0, 0);
 
-      // Proceed with the next iteration immediately
-      continue;
-    }
+  //     if (next_initial_batch_time > nextDay) {
+  //       // Wait until the next day at 7:00 AM UTC
+  //       const timeUntilNextDay = nextDay - now;
+  //       console.log("Waiting until the next day at 7:00 AM UTC...");
+  //       await new Promise((resolve) => setTimeout(resolve, timeUntilNextDay));
+  //     }
 
-    // Step 3: Call start_pipeline again
-  } while (should_auto_update);
+  //     // Proceed with the next iteration immediately
+  //     continue;
+  //   }
+
+  //   // Step 3: Call start_pipeline again
+  // } while (should_auto_update);
 
   should_auto_update = true;
 }
