39d861c8eac46eaffeb37cd18e0866f69e539f91
thevasanthram <vasanthram227@gmail.com>
2024-01-16 00:00:31 +0530

    optimized flashing ready to push

--- optimized_backend.js
+++ optimized_backend.js
@@ -2990,10 +2990,14 @@ async function data_processor(data_lake, sql_request, table_list) {
                     "data_pool_temp: after processing ",
                     data_pool_temp
                   );
+
+                  gross_pay_data = [...gross_pay_data, ...data_pool_temp];
+
+                  console.log('gross_pay_data: ', gross_pay_data)
                 }
 
                 if (data_pool_temp) {
-                  gross_pay_data.push(...data_pool_temp);
+                  // gross_pay_data = [...gross_pay_data, ...data_pool_temp];
                 }
               })
             );
