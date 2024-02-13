b9eef45dfc8a920e6302c7efef082d39c7e24715
thevasanthram <vasanthram227@gmail.com>
2024-02-13 13:04:18 +0530

    as of date

--- hvac_optimized_flasing.js
+++ hvac_optimized_flasing.js
@@ -63,7 +63,7 @@ createdBeforeTime.setUTCHours(7, 0, 0, 0);
 
 const params_header = {
   modifiedOnOrAfter: "", // 2024-01-17T00:00:00.00Z
-  modifiedBefore: "2024-02-11T00:00:00.00Z", //createdBeforeTime.toISOString()
+  modifiedBefore: "2024-02-12T00:00:00.00Z", //createdBeforeTime.toISOString()
   includeTotal: true,
   pageSize: 2000,
   active: "any",
