882847ff0965cb149d04d24801788d326415dde7
thevasanthram <vasanthram227@gmail.com>
2024-04-22 18:09:57 +0530

    cogs_labor properties added

--- wip/wip_report.js
+++ wip/wip_report.js
@@ -205,11 +205,11 @@ async function wip_report(as_of_date) {
 }
 
 async function wip_historical_report() {
-  let to_date = new Date("2024-01-01");
+  let to_date = new Date("2024-01-20");
   //   const to_dateString = to_date.toISOString().substring(0, 10);
   to_date = new Date(to_date);
 
-  const current_date = new Date("2024-04-19");
+  const current_date = new Date("2024-04-22");
 
   while (to_date <= current_date) {
     console.log("=========================================");
