435d4831cee7ab06c6dc5de72ee678a40a454e6f
thevasanthram <vasanthram227@gmail.com>
2024-06-04 19:19:25 +0530

    till date data

--- cogs_labor/cogs_labor_validator.js
+++ cogs_labor/cogs_labor_validator.js
@@ -45,13 +45,13 @@ async function validator() {
     );
     const hvac_db_result = hvac_db_response.recordset;
 
-    fs.writeFile("./main_db_result.js", JSON.stringify(main_db_result), () =>
-      console.log("main_db_result done")
-    );
+    // fs.writeFile("./main_db_result.js", JSON.stringify(main_db_result), () =>
+    //   console.log("main_db_result done")
+    // );
 
-    fs.writeFile("./hvac_db_result.js", JSON.stringify(hvac_db_result), () =>
-      console.log("hvac_db_result done")
-    );
+    // fs.writeFile("./hvac_db_result.js", JSON.stringify(hvac_db_result), () =>
+    //   console.log("hvac_db_result done")
+    // );
 
     console.log("main_db_result: ", main_db_result.length);
     console.log("hvac_db_result: ", hvac_db_result.length);
@@ -59,35 +59,41 @@ async function validator() {
     let count = 0;
 
     // Find records in main_db_result that are not in hvac_db_result
-    // const unique_main_db_result = main_db_result.filter((main_record) => {
-    //   count = count + 1;
-    //   console.log("count: ", count);
-    //   return !hvac_db_result.some((hvac_record) => {
-    //     return (
-    //       hvac_record["paid_duration"] == main_record["paid_duration"] &&
-    //       hvac_record["labor_cost"] == main_record["labor_cost"] &&
-    //       hvac_record["activity"] == main_record["activity"] &&
-    //       hvac_record["paid_time_type"] == main_record["paid_time_type"] &&
-    //       hvac_record["date"].getTime() == main_record["date"].getTime() &&
-    //       hvac_record["startedOn"].getTime() ==
-    //         main_record["startedOn"].getTime() &&
-    //       hvac_record["endedOn"].getTime() ==
-    //         main_record["endedOn"].getTime() &&
-    //       hvac_record["isPrevailingWageJob"] ==
-    //         main_record["isPrevailingWageJob"] &&
-    //       hvac_record["job_details_id"] == main_record["job_details_id"] &&
-    //       hvac_record["invoice_id"] == main_record["invoice_id"] &&
-    //       hvac_record["project_id"] == main_record["project_id"] &&
-    //       hvac_record["payrollId"] == main_record["payrollId"] &&
-    //       hvac_record["technician_id"] == main_record["technician_id"]
-    //     );
-    //   });
-    // });
+    const unique_main_db_result = hvac_db_result.filter((main_record) => {
+      count = count + 1;
+      console.log("count: ", count);
+      return !main_db_result.some((hvac_record) => {
+        return (
+          hvac_record["paid_duration"] == main_record["paid_duration"] &&
+          hvac_record["labor_cost"] == main_record["labor_cost"] &&
+          hvac_record["activity"] == main_record["activity"] &&
+          hvac_record["paid_time_type"] == main_record["paid_time_type"] &&
+          hvac_record["date"].getTime() == main_record["date"].getTime() &&
+          hvac_record["startedOn"].getTime() ==
+            main_record["startedOn"].getTime() &&
+          hvac_record["endedOn"].getTime() ==
+            main_record["endedOn"].getTime() &&
+          hvac_record["isPrevailingWageJob"] ==
+            main_record["isPrevailingWageJob"] &&
+          hvac_record["job_details_id"] == main_record["job_details_id"] &&
+          hvac_record["invoice_id"] == main_record["invoice_id"] &&
+          hvac_record["project_id"] == main_record["project_id"] &&
+          hvac_record["payrollId"] == main_record["payrollId"] &&
+          hvac_record["technician_id"] == main_record["technician_id"]
+        );
+      });
+    });
 
-    // console.log(
-    //   "Unique records in main_db_result: ",
-    //   unique_main_db_result.length
-    // );
+    console.log(
+      "Unique records in main_db_result: ",
+      unique_main_db_result.length
+    );
+
+    fs.writeFile(
+      "./cogs_labor_result.js",
+      JSON.stringify(unique_main_db_result),
+      () => console.log("cogs_labor_result done")
+    );
   } catch (err) {
     console.log("Error executing query: ", err);
   } finally {
