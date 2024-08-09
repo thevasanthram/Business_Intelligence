f033a736094cb0b264fc8a44547e12129925d601
thevasanthram <vasanthram227@gmail.com>
2024-08-09 13:20:42 +0530

    wip report

---
+++ modules/get_custom_wip_report_data.js
@@ -0,0 +1,263 @@
+const fs = require("fs");
+const path = require("path");
+const extractMatchingValues = require("./extract_matching_values");
+const { exit } = require("process");
+
+async function add_suffix(id, instance_id) {
+  let suffix = "";
+
+  // Append suffix based on instance_id
+  if (instance_id == 1) {
+    suffix = "_01";
+  } else if (instance_id == 2) {
+    suffix = "_02";
+  } else if (instance_id == 3) {
+    suffix = "_03";
+  } else if (instance_id == 4) {
+    suffix = "_04";
+  } else if (instance_id == 5) {
+    suffix = "_05";
+  }
+
+  // Append suffix to id
+  let modified_id = id + suffix;
+
+  // Return the modified id
+  return modified_id;
+}
+
+async function get_custom_wip_report_data(
+  access_token,
+  app_key,
+  instance_name,
+  tenant_id,
+  wip_report_id,
+  as_of_date,
+  data_pool,
+  page_count
+) {
+  let has_error_occured = false;
+
+  const wip_header_data = {
+    Instance_id: "",
+    ProjectId: "",
+    ProjectNumber: "",
+    ProjectName: "",
+    ProjectStatus: "",
+    ProjectContractStartDate: "",
+    ActualCompletionDate: "",
+    ContractValue: "",
+    ChangeOrderValue: "",
+    Retainage: "",
+    TDRetainage: "",
+    CostAdjustment: "",
+    TotalEstimatedCost: "",
+    EstimatedMargin: "",
+    EstimatedMarginPercentage: "",
+    TotalCost: "",
+    CostToComplete: "",
+    PercentCompleteCost: "",
+    EarnedRevenue: "",
+    TotalRevenue: "",
+    OverBilling: "",
+    UnderBilling: "",
+    OriginalEstimateTemplate: "",
+    ActualLaborQty: "",
+    LastInvoiceInvoicedOn: "",
+    TDOriginalEstimatedMargin: "",
+    TDOriginalEstimatedMarginPercentage: "",
+    ProjectBusinessUnit: "",
+    TDInvoiceAndActualLaborCostPlusBurdenCost: "",
+    PermitNumber: "",
+    UTC_update_date: "",
+  };
+
+  const instance_list = [
+    "Expert Heating and Cooling Co LLC",
+    "PARKER-ARNTZ PLUMBING AND HEATING, INC.",
+    "Family Heating & Cooling Co LLC",
+    "Swift Air Mechanical LLC",
+    "Jetstream Mechanicals LLC",
+  ];
+
+  try {
+    let shouldIterate = false;
+    do {
+      const api_endpoint = `https://api.servicetitan.io/reporting/v2/tenant/${tenant_id}/report-category/operations/reports/${wip_report_id}/data`;
+
+      const api_url =
+        api_endpoint + `?page=${page_count + 1}&includeTotal=true`;
+
+      const parameters = [
+        {
+          name: "From",
+          value: "1900-01-01",
+        },
+        {
+          name: "To",
+          value: as_of_date,
+        },
+        {
+          name: "TransactionAsOfDateOptions",
+          value: "E",
+        },
+        {
+          name: "TransactionAsOfDate",
+          value: as_of_date,
+        },
+        {
+          name: "InvoiceCostStatus",
+          value: "2",
+        },
+        {
+          name: "TransactionDateType",
+          value: "I",
+        },
+        {
+          name: "InvoiceRevenueStatus",
+          value: "2",
+        },
+        {
+          name: "ReturnSyncStatus",
+          value: "2",
+        },
+        {
+          name: "ReturnStatus",
+          value: "R",
+        },
+        {
+          name: "VendorBillStatus",
+          value: "2",
+        },
+        {
+          name: "VendorCostType",
+          value: "B",
+        },
+      ];
+
+      const request = await fetch(api_url, {
+        method: "POST",
+        headers: {
+          "Content-type": "application/json",
+          Authorization: access_token, // Include "Bearer" before access token
+          "ST-App-Key": app_key,
+        },
+        body: JSON.stringify({
+          parameters: parameters,
+        }),
+      });
+
+      const response = await request.json();
+
+      if (response["status"] == 429) {
+        // wait for specified seconds
+        waiting_time = parseInt(response["title"].match(/\d+/)[0]) + 3;
+
+        console.log(instance_name, response["title"]);
+
+        await (async () => {
+          await new Promise((resolve) =>
+            setTimeout(resolve, waiting_time * 1000)
+          );
+          shouldIterate = true;
+        })();
+
+        continue;
+      }
+
+      const fields = response.fields;
+      const data = response.data;
+
+      shouldIterate = response["hasMore"];
+
+      const pushing_item = [];
+
+      // console.log("response: ", response);
+
+      const updatedData = await Promise.all(
+        data.map(async (record, index) => {
+          // organizing data with their fields
+          const formatted_record = fields.reduce((obj, field, index) => {
+            obj[field.name] = record[index];
+            return obj;
+          }, {});
+
+          const instance_id = String(instance_list.indexOf(instance_name) + 1);
+
+          if (formatted_record["ProjectId"]) {
+            formatted_record["ProjectId"] = await add_suffix(
+              formatted_record["ProjectId"],
+              instance_id
+            );
+          }
+
+          if (formatted_record["ProjectNumber"]) {
+            formatted_record["ProjectNumber"] = await add_suffix(
+              formatted_record["ProjectNumber"],
+              instance_id
+            );
+          }
+
+          if (formatted_record["ProjectStartDate"]) {
+            formatted_record["ProjectContractStartDate"] =
+              formatted_record["ProjectStartDate"];
+
+            delete formatted_record["ProjectStartDate"];
+          }
+          if (instance_id == 1 || instance_id == 4) {
+            // EXP and SFT
+            formatted_record["EstimatedMargin"] =
+              formatted_record["ContractValue"] -
+              formatted_record["TotalEstimatedCost"];
+          }
+
+          if (formatted_record["PermitNumber(s)(separatepermitsusingcommas)"]) {
+            formatted_record["PermitNumber"] =
+              formatted_record["PermitNumber(s)(separatepermitsusingcommas)"];
+            delete formatted_record[
+              "PermitNumber(s)(separatepermitsusingcommas)"
+            ];
+          }
+
+          formatted_record["Instance_id"] = instance_id;
+          formatted_record["UTC_update_date"] = as_of_date;
+
+          const final_record = extractMatchingValues(
+            wip_header_data,
+            formatted_record
+          );
+
+          // console.log("final_record: ", final_record);
+          return final_record;
+        })
+      );
+
+      // fs.writeFile(
+      //   `./${instance_name}_wip_report.js`,
+      //   JSON.stringify(updatedData),
+      //   () => console.log("done")
+      // );
+
+      data_pool = [...data_pool, ...updatedData];
+
+      console.log(instance_name, data_pool.length, "/", response["totalCount"]);
+
+      page_count = page_count + 1;
+    } while (shouldIterate);
+  } catch (error) {
+    console.error(
+      `WIP Report fetching failed for ${instance_name} - ${as_of_date}. Try Again!:`,
+      error
+    );
+
+    has_error_occured = true;
+  }
+
+  return {
+    data_pool,
+    page_count,
+    has_error_occured,
+  };
+}
+
+module.exports = get_custom_wip_report_data;
---
+++ wip/custom_wip_report.js
@@ -0,0 +1,509 @@
+const getAccessToken = require("./../modules/get_access_token");
+const get_custom_wip_report_data = require("./../modules/get_custom_wip_report_data");
+const hvac_data_insertion = require("./../modules/hvac_data_insertion");
+const create_sql_connection = require("./../modules/create_sql_connection");
+const fs = require("fs");
+
+const data_lake = {};
+
+const instance_details = [
+  {
+    instance_name: "Expert Heating and Cooling Co LLC",
+    tenant_id: 1011756844,
+    app_key: "ak1.ztsdww9rvuk0sjortd94dmxwx",
+    client_id: "cid.jk53hfwwcq6a1zgtbh96byil4",
+    client_secret: "cs1.2hdc1yd19hpxzmdeg5rfuc6i3smpxy9iei0yhq1p7qp8mwyjda",
+    wip_report_id: 87933193,
+    active_project_id: 91683578,
+    completed_projects_id: 91678548,
+  },
+  {
+    instance_name: "PARKER-ARNTZ PLUMBING AND HEATING, INC.",
+    tenant_id: 1475606437,
+    app_key: "ak1.w9fgjo8psqbyi84vocpvzxp8y",
+    client_id: "cid.r82bhd4u7htjv56h7sqjk0jya",
+    client_secret: "cs1.4q3yjgyhjb9yaeietpsoozzc8u2qgw80j8ze43ovz1308e7zz7",
+    wip_report_id: 85353848,
+    active_project_id: 94758741,
+    completed_projects_id: 94755857,
+  },
+  {
+    instance_name: "Family Heating & Cooling Co LLC",
+    tenant_id: 1056112968,
+    app_key: "ak1.h0wqje4yshdqvn1fso4we8cnu",
+    client_id: "cid.qlr4t6egndd4mbvq3vu5tef11",
+    client_secret: "cs1.v9jhueeo6kgcjx5in1r8716hpnmuh6pbxiddgsv5d3y0822jay",
+    wip_report_id: 75359909,
+    active_project_id: 79737287,
+    completed_projects_id: 79746676,
+  },
+  {
+    instance_name: "Swift Air Mechanical LLC",
+    tenant_id: 2450322465,
+    app_key: "ak1.bquspwfwag2gqtls6lgi0dl1b",
+    client_id: "cid.ls3q3h7dmtoaiu3o5y5oddgca",
+    client_secret: "cs1.75f7fqxtilh4xj5vkym3fda1aaz9kmpv701w91kaej9w2r2rxp",
+    wip_report_id: 58999783,
+    active_project_id: 72752883,
+    completed_projects_id: 72763695,
+  },
+  {
+    instance_name: "Jetstream Mechanicals LLC",
+    tenant_id: 2450309401,
+    app_key: "ak1.u9fb0767d46nh1mid81ow3pgz",
+    client_id: "cid.my53hbp127i8vzxpn4o4h54ho",
+    client_secret: "cs1.3t0bo8k8b8xgrnbdyf9j4cj8zeq2upq2z72x9h4wkmf1w692jc",
+    wip_report_id: 180419843,
+    active_project_id: 258292875,
+    completed_projects_id: 258288362,
+  },
+];
+
+const wip_header = {
+  wip_active_projects: {
+    columns: {
+      Instance_id: {
+        data_type: "NVARCHAR20",
+        constraint: { nullable: false },
+      },
+      ProjectId: {
+        data_type: "NVARCHAR20",
+        constraint: { nullable: false },
+      },
+      ProjectNumber: {
+        data_type: "NVARCHAR20",
+        constraint: { nullable: false },
+      },
+      ProjectName: {
+        data_type: "NVARCHAR",
+        constraint: { nullable: true },
+      },
+      ProjectStatus: {
+        data_type: "NVARCHAR",
+        constraint: { nullable: true },
+      },
+      ProjectContractStartDate: {
+        data_type: "DATETIME2",
+        constraint: { nullable: true },
+      },
+      ActualCompletionDate: {
+        data_type: "DATETIME2",
+        constraint: { nullable: true },
+      },
+      ContractValue: {
+        data_type: "DECIMAL",
+        constraint: { nullable: true },
+      },
+      ChangeOrderValue: {
+        data_type: "DECIMAL",
+        constraint: { nullable: true },
+      },
+      Retainage: {
+        data_type: "DECIMAL",
+        constraint: { nullable: true },
+      },
+      TDRetainage: {
+        data_type: "DECIMAL",
+        constraint: { nullable: true },
+      },
+      CostAdjustment: {
+        data_type: "DECIMAL",
+        constraint: { nullable: true },
+      },
+      TotalEstimatedCost: {
+        data_type: "DECIMAL",
+        constraint: { nullable: true },
+      },
+      EstimatedMargin: {
+        data_type: "DECIMAL",
+        constraint: { nullable: true },
+      },
+      EstimatedMarginPercentage: {
+        data_type: "DECIMAL",
+        constraint: { nullable: true },
+      },
+      TotalCost: {
+        data_type: "DECIMAL",
+        constraint: { nullable: true },
+      },
+      CostToComplete: {
+        data_type: "DECIMAL",
+        constraint: { nullable: true },
+      },
+      PercentCompleteCost: {
+        data_type: "DECIMAL",
+        constraint: { nullable: true },
+      },
+      EarnedRevenue: {
+        data_type: "DECIMAL",
+        constraint: { nullable: true },
+      },
+      TotalRevenue: {
+        data_type: "DECIMAL",
+        constraint: { nullable: true },
+      },
+      OverBilling: {
+        data_type: "DECIMAL",
+        constraint: { nullable: true },
+      },
+      UnderBilling: {
+        data_type: "DECIMAL",
+        constraint: { nullable: true },
+      },
+      OriginalEstimateTemplate: {
+        data_type: "NVARCHAR",
+        constraint: { nullable: true },
+      },
+      ActualLaborQty: {
+        data_type: "DECIMAL",
+        constraint: { nullable: true },
+      },
+      LastInvoiceInvoicedOn: {
+        data_type: "DATETIME2",
+        constraint: { nullable: true },
+      },
+      TDOriginalEstimatedMargin: {
+        data_type: "DECIMAL",
+        constraint: { nullable: true },
+      },
+      TDOriginalEstimatedMarginPercentage: {
+        data_type: "DECIMAL",
+        constraint: { nullable: true },
+      },
+      ProjectBusinessUnit: {
+        data_type: "NVARCHAR",
+        constraint: { nullable: true },
+      },
+      TDInvoiceAndActualLaborCostPlusBurdenCost: {
+        data_type: "DECIMAL",
+        constraint: { nullable: true },
+      },
+      PermitNumber: {
+        data_type: "NVARCHAR",
+        constraint: { nullable: true },
+      },
+      UTC_update_date: {
+        data_type: "DATETIME2",
+        constraint: { nullable: true },
+      },
+    },
+  },
+
+  wip_completed_projects: {
+    columns: {
+      Instance_id: {
+        data_type: "NVARCHAR20",
+        constraint: { nullable: false },
+      },
+      ProjectId: {
+        data_type: "NVARCHAR20",
+        constraint: { nullable: false },
+      },
+      ProjectNumber: {
+        data_type: "NVARCHAR20",
+        constraint: { nullable: false },
+      },
+      ProjectName: {
+        data_type: "NVARCHAR",
+        constraint: { nullable: true },
+      },
+      ProjectStatus: {
+        data_type: "NVARCHAR",
+        constraint: { nullable: true },
+      },
+      ProjectContractStartDate: {
+        data_type: "DATETIME2",
+        constraint: { nullable: true },
+      },
+      ActualCompletionDate: {
+        data_type: "DATETIME2",
+        constraint: { nullable: true },
+      },
+      ContractValue: {
+        data_type: "DECIMAL",
+        constraint: { nullable: true },
+      },
+      ChangeOrderValue: {
+        data_type: "DECIMAL",
+        constraint: { nullable: true },
+      },
+      Retainage: {
+        data_type: "DECIMAL",
+        constraint: { nullable: true },
+      },
+      TDRetainage: {
+        data_type: "DECIMAL",
+        constraint: { nullable: true },
+      },
+      CostAdjustment: {
+        data_type: "DECIMAL",
+        constraint: { nullable: true },
+      },
+      TotalEstimatedCost: {
+        data_type: "DECIMAL",
+        constraint: { nullable: true },
+      },
+      EstimatedMargin: {
+        data_type: "DECIMAL",
+        constraint: { nullable: true },
+      },
+      EstimatedMarginPercentage: {
+        data_type: "DECIMAL",
+        constraint: { nullable: true },
+      },
+      TotalCost: {
+        data_type: "DECIMAL",
+        constraint: { nullable: true },
+      },
+      CostToComplete: {
+        data_type: "DECIMAL",
+        constraint: { nullable: true },
+      },
+      PercentCompleteCost: {
+        data_type: "DECIMAL",
+        constraint: { nullable: true },
+      },
+      EarnedRevenue: {
+        data_type: "DECIMAL",
+        constraint: { nullable: true },
+      },
+      TotalRevenue: {
+        data_type: "DECIMAL",
+        constraint: { nullable: true },
+      },
+      OverBilling: {
+        data_type: "DECIMAL",
+        constraint: { nullable: true },
+      },
+      UnderBilling: {
+        data_type: "DECIMAL",
+        constraint: { nullable: true },
+      },
+      OriginalEstimateTemplate: {
+        data_type: "NVARCHAR",
+        constraint: { nullable: true },
+      },
+      ActualLaborQty: {
+        data_type: "DECIMAL",
+        constraint: { nullable: true },
+      },
+      LastInvoiceInvoicedOn: {
+        data_type: "DATETIME2",
+        constraint: { nullable: true },
+      },
+      TDOriginalEstimatedMargin: {
+        data_type: "DECIMAL",
+        constraint: { nullable: true },
+      },
+      TDOriginalEstimatedMarginPercentage: {
+        data_type: "DECIMAL",
+        constraint: { nullable: true },
+      },
+      ProjectBusinessUnit: {
+        data_type: "NVARCHAR",
+        constraint: { nullable: true },
+      },
+      TDInvoiceAndActualLaborCostPlusBurdenCost: {
+        data_type: "DECIMAL",
+        constraint: { nullable: true },
+      },
+      PermitNumber: {
+        data_type: "NVARCHAR",
+        constraint: { nullable: true },
+      },
+      UTC_update_date: {
+        data_type: "DATETIME2",
+        constraint: { nullable: true },
+      },
+    },
+  },
+};
+
+const wip_response = {
+  wip_active_projects: {
+    "Expert Heating and Cooling Co LLC": {
+      status: "failure",
+    },
+    "PARKER-ARNTZ PLUMBING AND HEATING, INC.": {
+      status: "failure",
+    },
+    "Family Heating & Cooling Co LLC": {
+      status: "failure",
+    },
+    "Swift Air Mechanical LLC": {
+      status: "failure",
+    },
+    "Jetstream Mechanicals LLC": {
+      status: "failure",
+    },
+  },
+  wip_completed_projects: {
+    "Expert Heating and Cooling Co LLC": {
+      status: "failure",
+    },
+    "PARKER-ARNTZ PLUMBING AND HEATING, INC.": {
+      status: "failure",
+    },
+    "Family Heating & Cooling Co LLC": {
+      status: "failure",
+    },
+    "Swift Air Mechanical LLC": {
+      status: "failure",
+    },
+    "Jetstream Mechanicals LLC": {
+      status: "failure",
+    },
+  },
+};
+
+async function wip_report(as_of_date, column_name, wip_table_name) {
+  console.log("=========================================");
+  console.log("as of date: ", as_of_date);
+  console.log("=========================================");
+
+  await Promise.all(
+    instance_details.map(async (instance_data) => {
+      const instance_name = instance_data["instance_name"];
+      const tenant_id = instance_data["tenant_id"];
+      const app_key = instance_data["app_key"];
+      const client_id = instance_data["client_id"];
+      const client_secret = instance_data["client_secret"];
+      const wip_report_id = instance_data[column_name];
+
+      let access_token = "";
+
+      // refreshing for the first time manually
+      do {
+        access_token = await getAccessToken(client_id, client_secret);
+      } while (!access_token);
+
+      const refreshAccessToken = async () => {
+        // Signing a new access token in Service Titan's API
+        do {
+          access_token = await getAccessToken(client_id, client_secret);
+        } while (!access_token);
+      };
+
+      setInterval(refreshAccessToken, 1000 * 60 * 3);
+
+      // continuously fetching whole api data
+      let data_pool = [];
+      let page_count = 0;
+      let has_error_occured = false;
+
+      do {
+        ({ data_pool, page_count, has_error_occured } =
+          await get_custom_wip_report_data(
+            access_token,
+            app_key,
+            instance_name,
+            tenant_id,
+            wip_report_id,
+            as_of_date,
+            data_pool,
+            page_count
+          ));
+      } while (has_error_occured);
+
+      if (data_pool.length > 0) {
+        data_lake[instance_name] = data_pool;
+      }
+    })
+  );
+
+  // creating a client for azure sql database operations
+  let sql_request = "";
+  do {
+    sql_request = await create_sql_connection();
+  } while (!sql_request);
+
+  const header_data = wip_header[wip_table_name]["columns"];
+
+  await Promise.all(
+    Object.keys(data_lake).map(async (instance_name) => {
+      const data_pool = data_lake[instance_name];
+
+      // fs.writeFile(
+      //   `./${instance_name}_wip_report.js`,
+      //   JSON.stringify(data_pool),
+      //   () => console.log("done")
+      // );
+
+      do {
+        wip_response[wip_table_name][instance_name]["status"] =
+          await hvac_data_insertion(
+            sql_request,
+            data_pool,
+            header_data,
+            wip_table_name,
+            "UPDATING"
+          );
+      } while (
+        wip_response[wip_table_name][instance_name]["status"] != "success"
+      );
+    })
+  );
+}
+
+async function wip_historical_report() {
+  //   const to_dateString = to_date.toISOString().substring(0, 10);
+  const current_date = new Date("2024-08-01");
+
+  await wip_report(
+    current_date.toISOString().substring(0, 10),
+    "active_project_id",
+    "wip_active_projects"
+  );
+
+  await wip_report(
+    current_date.toISOString().substring(0, 10),
+    "completed_projects_id",
+    "wip_completed_projects"
+  );
+
+  current_date.setDate(current_date.getDate() + 1);
+
+  current_date.setUTCHours(7, 0, 0, 0);
+
+  let iterator = true;
+
+  do {
+    const now = new Date();
+
+    if (now < current_date) {
+      // Schedule the next call after an day
+      const timeUntilNextBatch = current_date - now; // Calculate milliseconds until the next day
+      console.log(
+        "timer funtion entering",
+        timeUntilNextBatch,
+        "<-->",
+        current_date
+      );
+
+      await new Promise((resolve) => setTimeout(resolve, timeUntilNextBatch));
+    } else {
+      await wip_report(
+        current_date.toISOString().substring(0, 10),
+        "active_project_id",
+        "wip_active_projects"
+      );
+
+      await wip_report(
+        current_date.toISOString().substring(0, 10),
+        "completed_projects_id",
+        "wip_completed_projects"
+      );
+
+      if (current_date.toISOString().substring(0, 10) == "01-01-2020") {
+        iterator = false;
+      } else {
+        current_date.setDate(current_date.getDate() + 1);
+        current_date.setUTCHours(7, 0, 0, 0);
+      }
+    }
+  } while (iterator);
+
+  console.log("COMPLETED");
+}
+
+wip_historical_report();
