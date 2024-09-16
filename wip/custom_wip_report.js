343ee90bca87dd4749c0021a22baa72dcac1a895
thevasanthram <vasanthram227@gmail.com>
2024-09-16 15:07:18 +0530

    custom wip report

--- wip/custom_wip_report.js
+++ wip/custom_wip_report.js
@@ -480,7 +480,7 @@ async function wip_report(as_of_date, column_name, wip_table_name) {
 async function wip_historical_report() {
   //   const to_dateString = to_date.toISOString().substring(0, 10);
   // 2022-10-09
-  const current_date = new Date("2024-05-01");
+  const current_date = new Date("2024-01-01");
   // const current_date = new Date();
 
   await wip_report(
@@ -529,7 +529,7 @@ async function wip_historical_report() {
       // );
 
       console.log("current_date.toISOString(): ", current_date.toISOString());
-      if (current_date.toISOString().substring(0, 10) == "2024-08-31") {
+      if (current_date.toISOString().substring(0, 10) == "2024-04-30") {
         iterator = false;
       } else {
         current_date.setDate(current_date.getDate() + 1);
